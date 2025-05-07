bfb5ceb89b3d58085bff32738dbf164d800a237c
Tiago Rech <rech.tiago94@gmail.com>
2025-05-07 13:52:01 -0300

    ci: fix

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -27,6 +27,13 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
   }
 };
 export default config;
